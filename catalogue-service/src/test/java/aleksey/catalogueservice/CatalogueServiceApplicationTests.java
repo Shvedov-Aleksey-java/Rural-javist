f51b5fcf0663719866ff9c8dcc97b090cbc2af47
79686 <mrak5455@yandex.ru>
2024-06-09 11:00:55 +0300

    начальная фиесацыя рест

---
+++ catalogue-service/src/test/java/aleksey/catalogueservice/CatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package aleksey.catalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
